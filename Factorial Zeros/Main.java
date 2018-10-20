0c1bba3c54530eca59aad4e46c76a4ee9cef7836
Alberto Mur Lopez <565825@unizar.es>
2018-10-20 23:27:08 +0200

    Factorial Zeros

---
+++ Factorial Zeros/Main.java
@@ -0,0 +1,27 @@
+// Don't place your source in a package
+import java.math.BigInteger;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// Please name your class Main
+class Main {
+
+    public static void main (String[] args) throws java.lang.Exception {
+        Scanner in = new Scanner(System.in);
+        in.nextInt();
+        int T = in.nextInt(), B, N;
+        BigInteger f;
+        HashMap<Integer, Integer> baseFactors;
+        for(int t=0; t<T; t++) {
+
+            B = in.nextInt();
+            N = in.nextInt();
+
+        }
+        int b = in.nextInt();
+        System.out.print(a + b);
+    }
+
+
+}
